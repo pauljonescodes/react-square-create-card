5789d0e5a1c48cf346d6d32bc3844e6b78f1b3b4
PLJNS <me@pljns.com>
2023-09-14 17:09:49 -0400

    Initialize project with Square

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# React Square Create Card
+
+1. `cp .env-example .env`
+2. Fill in your details
+3. Happy hacking!
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, Payments } from "@square/web-payments-sdk-types";
+import { payments } from "@square/web-sdk";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [paymentsState, setPaymentsState] = useState<Payments | null>(null);
+  const [cardState, setCardState] = useState<Card | null>(null);
+
+  useEffect(() => {
+    async function initializePayments() {
+      let loadedPayments: Payments | null;
+      if (!paymentsState) {
+        try {
+          loadedPayments = await payments(
+            process.env.REACT_APP_SQUARE_APP_ID!,
+            process.env.REACT_APP_SQUARE_LOCATION_ID!
+          );
+          if (loadedPayments != null) {
+            setPaymentsState(loadedPayments);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+    initializePayments();
+  }, [paymentsState]);
+
+  useEffect(() => {
+    let card: Card | undefined;
+    if (paymentsState && !cardState) {
+      try {
+        (async () => {
+          card = await paymentsState.card();
+          await card.attach("#card-container");
+          if (card != null) {
+            setCardState(card);
+          }
+        })();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    return () => {
+      cardState?.detach();
+    };
+  }, [paymentsState, cardState]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form id="payment-form">
+        <div id="card-container"></div>
+        <button
+          id="card-button"
+          type="button"
+          onClick={async () => {
+            const cardTokenizationResult = await cardState?.tokenize();
+            if (
+              cardTokenizationResult?.status !== "OK" ||
+              cardTokenizationResult?.token == null
+            ) {
+              throw new Error(
+                `Tokenization errors: ${JSON.stringify(
+                  cardTokenizationResult?.errors
+                )}`
+              );
+            }
+
+            const verifyBuyerResponseDetails = await paymentsState?.verifyBuyer(
+              cardTokenizationResult?.token,
+              {
+                billingContact: {
+                  givenName: process.env.REACT_APP_BUYER_GIVEN_NAME,
+                  familyName: process.env.REACT_APP_BUYER_FAMILY_NAME,
+                  addressLines: [
+                    process.env.REACT_APP_BUYER_ADDRESS_LINE_1!,
+                    process.env.REACT_APP_BUYER_ADDRESS_LINE_2!,
+                  ],
+                  city: process.env.REACT_APP_BUYER_CITY,
+                  state: process.env.REACT_APP_BUYER_STATE,
+                  postalCode: process.env.REACT_APP_BUYER_POSTAL_CODE,
+                  countryCode: process.env.REACT_APP_BUYER_COUNTRY_CODE,
+                  email: process.env.REACT_APP_BUYER_EMAIL,
+                  phone: process.env.REACT_APP_BUYER_PHONE,
+                },
+                intent: "STORE",
+              }
+            );
+
+            // Do something with:
+            console.log(cardTokenizationResult?.token); // "sourceId"
+            console.log(verifyBuyerResponseDetails?.token);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Store Card
+        </button>
+      </form>
     </div>
   );
 }
